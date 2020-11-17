4a6df24884a65f59cf4c7552eac0de8d0b24510c
Akinyosoye Gabriel <gabrielfemi799@gmail.com>
2020-11-17 11:42:19 +0100

    Add index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+echo "What is GIT?";
+// It is a version control system that helps you track your code.
+echo "Welcome to Git!";
+//username and password
+// Sign in with GMAIL
+// Referral
+// sign in with facebook
+// git init - create a new git repository
+// git add {nameOfFile} -> Make git aware of the file
+// git commit -> Commit the file to the repository
